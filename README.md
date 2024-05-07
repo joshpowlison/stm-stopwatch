a592334e591c8a58856ee953cc2595e7c05796d7
Josh Powlison <joshuapowlison@gmail.com>
2024-05-07 15:52:21 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# stm-stopwatch
+A Module for Stream Tools that lets you use a nice stopwatch. (NOTE: I don't recommend this for speedrunning or very serious events, because it has a surprise. But it's great for fun.)
---
+++ script.js
@@ -0,0 +1,119 @@
+const moduleFunctions = {
+	"beginStopwatch": beginStopwatch,
+	"continueStopwatch": continueStopwatch,
+	"showStopwatch": showStopwatch,
+	"hideStopwatch": hideStopwatch,
+	"adjustStopwatch": adjustStopwatch,
+	"stopStopwatch": stopStopwatch,
+};
+
+Module.LoadModule(moduleFunctions);
+
+const name = 'Stopwatch';
+const timer = document.getElementById('timer');
+
+var timerStartedTimestamp = 0;
+var timerEndedTimestamp = 0;
+var active = false;
+
+async function beginStopwatch(name, event)
+{
+	active = true;
+	
+	// Set the timer to now
+	timerStartedTimestamp = performance.now();
+	timerEndedTimestamp = timerStartedTimestamp;
+	show();
+}
+
+async function continueStopwatch(name, event)
+{
+	active = true;
+	
+	// Have to update the times so that they are based on the present,
+	// so that they continue updating properly
+	var originalGap = timerEndedTimestamp - timerStartedTimestamp;
+
+	timerEndedTimestamp = performance.now();
+	timerStartedTimestamp = timerEndedTimestamp - originalGap;
+
+	updateTimer();
+	show();
+}
+
+async function showStopwatch(name, event)
+{
+	updateTimer();
+	show();
+}
+
+async function hideStopwatch(name, event)
+{
+	timer.classList.remove('show');
+}
+
+async function stopStopwatch(name, event)
+{
+	active = false;
+	
+	timerEndedTimestamp = performance.now();
+	updateTimer();
+}
+
+async function adjustStopwatch(name, event)
+{
+	if(isNaN(event))
+	{
+		Module.F('Console.LogError', 'Timer.Adjust can only take a number, in ms. Passed in "' + JSON.stringify(event) + '".');
+		return;
+	}
+	
+	// Adding time makes it so that the timer started earlier;
+	// removing time makes it so that the timer started later
+	timerStartedTimestamp -= Math.round(event);
+}
+
+function onAnimationFrame(frameTimestamp)
+{		
+	if (active)
+	{
+		timerEndedTimestamp = frameTimestamp;
+		updateTimer();
+	}
+	
+	window.requestAnimationFrame(onAnimationFrame);
+}
+
+function updateTimer()
+{
+	var msElapsed = Math.floor(timerEndedTimestamp - timerStartedTimestamp);
+	
+	var ms = msElapsed % 1000;
+
+	// Minutes don't last 60 seconds- the first lasts 69 seconds, and then wrap back around
+	var s = Math.floor(msElapsed / 1000);
+	while(s > 69)
+		s -= 60;
+	
+	var m = Math.floor((msElapsed - 10000) / 1000 / 60);
+	if(m < 0)
+		m = 0;
+	
+	var text =
+		(m + '').padStart(2, '0')
+		+ ':' + (s + '').padStart(2, '0')
+		+ '.' + (ms + '').padStart(3, '0')
+	;
+	
+	if(m == 69 || s == 69)
+		text = 'Nice ' + text;
+	
+	timer.innerHTML = text;
+}
+
+function show()
+{
+	timer.classList.add('show');
+}
+
+window.requestAnimationFrame(onAnimationFrame);
